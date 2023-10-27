358dcaa221d6e4fe30cd297fae8bbe14e75c2b96
anilmaurya61 <anilkumarmaurya246@gmail.com>
2023-10-27 15:39:13 +0530

    Update

--- client.js
+++ client.js
@@ -1,10 +1,68 @@
 const fetch = require("node-fetch");
 
-async function main() {
-  const response = await fetch("http://localhost:3000/todos?user_id=2");
-  const data = await response.json();
-  console.log(data);
-}
-main();
+async function fetchTodos(allUsers) {
+  let pr1 = new Promise(async (resolve, reject) => {
+    let allUserTodos = [];
+    for (let i = 0; i < 5; i++) {
+      let id = allUsers.users[i].id;
+      let userName = allUsers.users[i].name;
+      let usertodosdata = await fetch(`http://localhost:3000/todos?user_id=${id}`);
+      const usertodos = await usertodosdata.json();
+      allUserTodos.push({ 'id': id, 'name': userName, 'todos': usertodos.todos })
+    }
+    resolve(allUserTodos);
+  });
 
-// write your code here
+  let pr2 = new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      let allUserTodos = [];
+      for (let i = 5; i < 10; i++) {
+        let id = allUsers.users[i].id;
+        let userName = allUsers.users[i].name;
+        let usertodosdata = await fetch(`http://localhost:3000/todos?user_id=${id}`);
+        const usertodos = await usertodosdata.json();
+        allUserTodos.push({ 'id': id, 'name': userName, 'todos': usertodos.todos })
+      }
+      resolve(allUserTodos);
+    }, 5);
+  });
+
+  let pr3 = new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      let allUserTodos = [];
+      for (let i = 10; i < 15; i++) {
+        let id = allUsers.users[i].id;
+        let userName = allUsers.users[i].name;
+        let usertodosdata = await fetch(`http://localhost:3000/todos?user_id=${id}`);
+        const usertodos = await usertodosdata.json();
+        allUserTodos.push({ 'id': id, 'name': userName, 'todos': usertodos.todos })
+      }
+      resolve(allUserTodos);
+    }, 2);
+  });
+
+  return Promise.all([pr1, pr2, pr3]);
+}
+
+async function main() {
+  const response = await fetch("http://localhost:3000/users");
+  const users = await response.json();
+
+  const [p1, p2, p3] = await fetchTodos(users);
+  let allTodos = [];
+  allTodos.push(p1, p2, p3);
+  allTodos = allTodos.flat();
+  const results = [];
+  allTodos.forEach((todo, index) => {
+    const numTodosCompleted = todo.todos.filter(todo => todo.isCompleted).length;
+
+    results.push({
+      id: todo.id,
+      name: todo.name,
+      numTodosCompleted: numTodosCompleted,
+    });
+  });
+  console.log(results);
+}
+
+main();
